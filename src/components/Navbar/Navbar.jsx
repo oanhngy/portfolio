b5c041f68f4ec0d7ec67fee65b80403e4d5217c7
oanh <oanhnguyenhma@gmail.com>
2026-06-23 23:32:41 +0700

    feat: build navbar with mobile hamburger toggle

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+import styles from './Navbar.module.css';
+
 function Navbar() {
-    return <section>Navbar</section>
+    const [isOpen, setIsOpen]= useState(false);
+
+    return (
+    <nav className={styles.navbar}>
+        <div className={styles.container}>
+            <a href="#home" className={styles.logo}>Oanh Ng.</a>
+            {/* web only */}
+            <ul className={`${styles.navLinks} ? ${isOpen ? styles.navLinksOpen : ''}`}>
+                <li><a href="#home">// Home</a></li>
+                <li><a href="#about">// About</a></li>
+                <li><a href="#expertise">// Skills</a></li>
+                <li><a href="#projects">// Projects</a></li>
+                <li><a href="#contact">// Contact</a></li>
+            </ul>
+
+            {/* mobile only */}
+            <button className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'x' : '☰'}</button>
+        </div>
+    </nav>
+    )
 }
 
 export default Navbar
