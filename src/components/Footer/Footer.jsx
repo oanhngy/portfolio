77b410ef3733d32c0d16586403e546130d8561c6
oanh <oanhnguyenhma@gmail.com>
2026-06-24 17:31:16 +0700

    feat: build Footer with scrollToTop button and bigName decoration

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,21 @@
+import Contact from '../Contact/Contact'
+import styles from './Footer.module.css'
+
 function Footer() {
-    return <section>Footer</section>
+    function scrollToTop() {
+        window.scrollTo({ top:0, behavior: 'smooth'})
+    }
+
+    return (
+        <footer className={styles.footer}>
+            <Contact />
+            <div className={styles.bottom}>
+                <p className={styles.copyright}>©2026. Made by Oanh Nguyen. All right reserved.</p>
+            </div>
+            <button className={styles.scrollToTop} onClick={scrollToTop}>↑</button>
+            <span className={styles.bigName}>OA</span>
+        </footer>
+    )
 }
 
 export default Footer
