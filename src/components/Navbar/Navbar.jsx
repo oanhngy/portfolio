f0da202d46340f583c2dca8344768e8f0fd2aae7
oanh <oanhnguyenhma@gmail.com>
2026-06-24 17:20:51 +0700

    fix: correct navLinks className and extend hamburger to tablet

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
         <div className={styles.container}>
             <a href="#home" className={styles.logo}>Oanh Ng.</a>
             {/* web only */}
-            <ul className={`${styles.navLinks} ? ${isOpen ? styles.navLinksOpen : ''}`}>
+            <ul className={`${styles.navLinks} ${isOpen ? styles.navLinksOpen : ''}`}>
                 <li><a href="#home">// Home</a></li>
                 <li><a href="#about">// About</a></li>
                 <li><a href="#expertise">// Skills</a></li>
