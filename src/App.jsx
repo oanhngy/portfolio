ffd59557896c6ba82de85e5993bf3d89a2cd0282
oanh <oanhnguyenhma@gmail.com>
2026-06-28 00:45:59 +0700

    refactor: remove Expertise component, replaced by Skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import AboutMe from './components/AboutMe/AboutMe'
-import Expertise from './components/Expertise/Expertise'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 import Navbar from './components/Navbar/Navbar'
 import Projects from './components/Projects/Projects'
+import Skills from './components/Skills/Skills'
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <main>
         <Home />
         <AboutMe />
-        <Expertise />
+        <Skills />
         <Projects />
       </main>
 
--- src/components/Expertise/Expertise.jsx
+++
@@ -1,5 +0,0 @@
-function Expertise() {
-    return <section id="expertise">Expertise</section>
-}
-
-export default Expertise
