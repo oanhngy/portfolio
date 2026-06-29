2ac01f9212154eb871dee8b2254e7b57878d8dcb
oanh <oanhnguyenhma@gmail.com>
2026-06-29 15:15:05 +0700

    fix: update Skills nav link href from #expertise to #skills

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
             <ul className={`${styles.navLinks} ${isOpen ? styles.navLinksOpen : ''}`}>
                 <li><a href="#home">// Home</a></li>
                 <li><a href="#about">// About</a></li>
-                <li><a href="#expertise">// Skills</a></li>
+                <li><a href="#skills">// Skills</a></li>
                 <li><a href="#projects">// Projects</a></li>
                 <li><a href="#contact">// Contact</a></li>
             </ul>
