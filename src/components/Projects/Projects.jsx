9c5230bd39feb9e7f85e82c78a9055d1edcc02b0
oanh <oanhnguyenhma@gmail.com>
2026-07-06 17:34:08 +0700

    feat: build Projects section with hover effects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,38 @@
+import projects from '../../data/projects'
+import SectionTitle from '../SectionTitle/SectionTitle'
+import styles from './Projects.module.css'
+
 function Projects() {
-    return <section id="projects">Project</section>
+    return (
+        <section id="projects" className={styles.section}>
+            <SectionTitle style>My Projects</SectionTitle>
+            <div className={styles.gridContainer}>
+                {projects.map(project => (
+                    <a
+                        key={project.id}
+                        href={project.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={styles.card}
+                    >
+                        <img src={project.img} alt={project.title} className={styles.img} />
+                        <h3 className={styles.title}>{project.title}</h3>
+                        
+                        <div className={styles.cardBottom}>
+                            <div className={styles.tags}>
+                            {project.tech.map(tag => (
+                                <span key={tag} className={styles.tag}>{tag}</span>
+                            ))}
+                            </div>
+                            <div className={styles.showProject}>
+                                <span>Show Project →</span>
+                            </div>
+                        </div>
+                    </a>
+                ))}
+            </div>
+        </section>
+    )
 }
 
 export default Projects
---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+    {
+        id: "project-1",
+        title: "Personal Portfolio Website",
+        img: "/project3.jpeg",
+        tech: ["HTML", "CSS", "JavaScript", "React", "Vite"],
+        github: "https://github.com/oanhngy/Web-Bookstore-API-Refactor-v1",
+    },
+    {
+        id: "project-2",
+        title: "Bookstore API - Refactored Architecture",
+        img: "/project3.jpeg",
+        tech: ["ASP.NET Core Web API", "C#", "MySQL", "xUnit", "Moq", "JWT"],
+        github: "https://github.com/oanhngy/Web-Bookstore-API-Refactor-v1",
+    },
+    {
+        id: "project-3",
+        title: "Bookstore Web Application",
+        img: "/project3.jpeg",
+        tech: ["ASP.NET Core MVC", "C#", "MySQL", "EF Core"],
+        github: "https://github.com/oanhngy/Web-Bookstore",
+    },
+    {
+        id: "project-4",
+        title: "World Cup Simulation - Testing project",
+        tech: ["Java", "JUnit", "Maven"],
+        img: "/project3.jpeg",
+        github: "https://github.com/oanhngy/Testing-WorldCup",
+    },
+]
+
+export default projects
